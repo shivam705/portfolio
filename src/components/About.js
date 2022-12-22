0feb11436cd4f012296f43752d30d82d18925eb3
Shivam Kushwaha <shivam.kushwaha@nagarro.com>
2022-12-22 11:45:56 +0530

    second commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ export default function About(){
           <div >
             <div style={{backgroundColor: "lightblue", textAlign:"center",width:"100%",height:"31em"}}>
             <p>hellos</p>
+              <h1>Shivam Kushwaha.</h1><br/>
               <h1>Shivam Kushwaha.</h1>
               <h1>I build things for the web.</h1>
               I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences.<br/> 
